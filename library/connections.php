f94dbb3a5f06d2e9de0739824ebeb7e71e2a5823
Colter-Hammer <therondowdle7@gmail.com>
2018-10-07 00:16:35 -0600

    Enhancement 2 finished

---
+++ library/connections.php
@@ -0,0 +1,21 @@
+<?php
+function acmeConnect(){
+    $server = "localhost";
+    $database = "acme";
+    $username = "Client1";
+    $password = "swimjumpwealthflavor";
+    $dsn = 'mysql:host=' .$server .';dbname=' . $database;
+    $options = array(PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION);
+
+  // Create the actual connection object and assign it to a variable
+ try {
+  $link = new PDO($dsn, $username, $password, $options);
+  return $link;
+ } catch(PDOException $e) {
+  header('Location: /acme/view/500.php');
+  exit;
+ }
+}
+
+/* testing */
+acmeConnect();
---
+++ view/500.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>Acme</title>
+        <link rel="stylesheet" type="text/css" href="../css/main.css">
+    </head>
+    
+    <body>
+        <div id="full-body-wrapper">
+    <header id="page-header">
+      <img id="logo-img" src="../images/site/logo.gif" alt="Acme logo">
+<span id="my-account">
+    <a href="/" alt="my-account" target="_blank"><img src="../images/site/account.gif" alt="My Account">My Account</a></span>
+    </header>
+     <nav id="navigation">
+      <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/nav.php'; ?>
+    </nav>
+            <main>
+      <div id="home-container">
+                <h1>Server Error</h1>
+                <h3>Sorry, the server experienced a problem.</h3>
+                                
+            </main>
+       <div id="line-break"></div>
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/footer.php'; ?>
+    </footer>
+    </div>
+    </body>
+</html>
